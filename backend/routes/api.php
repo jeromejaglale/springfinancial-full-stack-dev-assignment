bca1ff6252d3efb0cc750c32ee53bcdbe29ad3fe
Jerome Jaglale <jerome_jaglale@sfu.ca>
2023-09-14 20:13:19 -0700

    Add user deletion

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,10 +4,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
 
+// user list
 Route::get('/user', function () {
 	return User::all();
 });
 
+// create user
 Route::post('/user', function (Request $request) {
 	$user_data = $request->json()->all();
 
@@ -16,4 +18,17 @@ Route::post('/user', function (Request $request) {
 	return $user;
 });
 
+// delete user
+Route::delete('/user/{user_id}', function ($user_id) {
+	$user = User::where('id', $user_id)->first();
+
+	if($user == null) {
+		abort(404);
+	}
+
+	$user->delete();
+});
+
+
 // curl -H "Content-type: application/json" -d '{"name": "user test 1", "age":36, "address": "345 Madison Ave"}' http://springfinancial.local/api/user
+// curl -X DELETE http://springfinancial.local/api/user/2

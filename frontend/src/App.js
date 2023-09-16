4319f99967ad04dc42ceec92d5a58d91ed6b4a84
Jerome Jaglale <jerome_jaglale@sfu.ca>
2023-09-16 15:29:41 -0700

    Add list of users

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,62 @@
 import React from 'react';
+import useSWR from 'swr';
+
+async function fetcher(endpoint) {
+  const response = await fetch(endpoint);
+  const json = await response.json();
+  return json;
+}
+
+const ENDPOINT = 'http://springfinancial.local/api/user';
 
 export function App() {
+  // status: idle | loading | success | error
+  const [status, setStatus] = React.useState('idle');
+	const { data, isLoading, error } = useSWR(ENDPOINT, fetcher);
+  
   return (
-		<>
-		  <h1>Hello world</h1>
-		  <p>ok</p>
-		</>
+    <>
+	    {isLoading && (
+		    <p>Loading users…</p>
+		  )}
+
+	    {error && (
+		    <p>Sorry, the users could not be retrieved.</p>
+		  )}
+
+			{data && (
+	      <>
+					<table>
+						<thead>
+							<tr>
+								<th></th>
+								<th>Name</th>
+								<th></th>
+								<th></th>
+								<th>Points</th>
+							</tr>
+						</thead>
+						<tbody>
+							{data.map(user =>
+								<tr key={user.id}>
+									<td>
+										<a href="">Delete</a>
+									</td>
+									<td>{user.name}</td>
+									<td>
+										<a href="">Add Point</a>
+									</td>
+									<td>
+										<a href="">Subtract Point</a>
+									</td>
+									<td>{user.points}</td>
+								</tr>
+							)}
+						</tbody>
+					</table>
+	      </>
+      )}
+
+    </>
   );
 }

a123c453ff23e6ea17de347fc68851815ee008c5
Jerome Jaglale <jerome_jaglale@sfu.ca>
2023-09-16 18:53:08 -0700

    Add create user form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import useSWR from 'swr';
+import { useSWRConfig } from 'swr';
 
 async function fetcher(endpoint) {
   const response = await fetch(endpoint);
@@ -14,6 +15,41 @@ export function App() {
   const [status, setStatus] = React.useState('idle');
 	const { data, isLoading, error } = useSWR(ENDPOINT, fetcher);
   
+
+	// user form
+  const [name, setName] = React.useState('');
+  const [age, setAge] = React.useState();
+  const [address, setAddress] = React.useState('');
+
+  const { mutate } = useSWRConfig()
+
+  async function handleSubmit(e) {
+		e.preventDefault();
+
+		const data = {
+			name: name,
+			age: age,
+			address: address,
+		};
+
+		const response = await fetch(ENDPOINT, {
+		    method: 'POST',
+		    body: JSON.stringify(data),
+		  });
+
+		const json = await response.json();
+
+		// TODO error handling
+
+		// clear form
+		setName('');
+		setAge('');
+		setAddress('');
+
+		mutate(ENDPOINT);
+		// toggleProductForm();
+  }
+
   return (
     <>
 	    {isLoading && (
@@ -57,6 +93,21 @@ export function App() {
 	      </>
       )}
 
+		<h2>Add User</h2>
+	  <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} required />
+
+        <label htmlFor="age">Age:</label>
+        <input type="number" id="age" value={age} onChange={(e) => setAge(e.target.value)} />
+
+        <label htmlFor="address">Address:</label>
+        <input type="text" id="address" value={address} onChange={(e) => setAddress(e.target.value)} />
+
+        <p>
+          <input type="submit" value="Add User" />
+        </p>
+      </form>
     </>
   );
 }
